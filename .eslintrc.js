b112549d062ada8d1b500b740f47e24c6849d4a7
Paul Renault <paul.renault@espridigital.fr>
2023-03-27 16:55:44 +0200

    remove any rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
     '@typescript-eslint/semi': ['error', 'always'],
