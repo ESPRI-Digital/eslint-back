772f2d662573d5427f571e082a4a888968b5934b
Nicolas Melin <nicolas.melin@espridigital.fr>
2024-06-07 10:51:18 +0200

    Bump to 3.0.0-beta.1

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
+import antfu from '@antfu/eslint-config';
 import typescriptParser from "@typescript-eslint/parser"
 import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin"
 
-export default {
-    files: ["**/*.ts"],
-	ignores: ["eslint.config.js"],
-    languageOptions: {
-        parser: typescriptParser,
+export default antfu(
+    {
+        ignores: [
+        'src/*.d.ts',
+        ],
+    },
+    {
+        languageOptions: {
         parserOptions: {
-            project: 'tsconfig.json',
-            sourceType: 'module',
-        }
-    },
-    plugins: {
+            project: ['tsconfig.json'],
+        },
+        parser: typescriptParser,
+        },
+        plugins: {
         '@typescript-eslint': typescriptEslintPlugin,
-    },
-    rules: {
+        },
+        rules: {
         ...typescriptEslintPlugin.configs.recommended.rules,
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+        'style/semi': ['error', 'always'],
         '@typescript-eslint/semi': ['error', 'always'],
+        '@typescript-eslint/no-floating-promises': 'error',
+        'no-async-promise-executor': 'error',
+        'no-await-in-loop': 'error',
+        'no-promise-executor-return': 'error',
+        'no-return-await': 'error',
+        '@typescript-eslint/await-thenable': 'error',
+        '@typescript-eslint/no-misused-promises': 'error',
+        '@typescript-eslint/promise-function-async': 'error',
+        'style/member-delimiter-style': [
+            'error',
+            {
+            multiline: {
+                delimiter: 'semi',    // 'none' or 'semi' or 'comma'
+                requireLast: true,
+            },
+            }
+        ],
+        'curly': ['error', 'all'],
         '@typescript-eslint/indent': [
             'error',
             2,
             {
-                SwitchCase: 1,
-                VariableDeclarator: 1,
-                outerIIFEBody: 1,
-                MemberExpression: 1,
-                FunctionDeclaration: { parameters: 1, body: 1 },
-                FunctionExpression: { parameters: 1, body: 1 },
-                CallExpression: { arguments: 1 },
-                ArrayExpression: 1,
-                ObjectExpression: 1,
-                ImportDeclaration: 1,
-                flatTernaryExpressions: false,
-                ignoreComments: false,
-                ignoredNodes: [
+            SwitchCase: 1,
+            VariableDeclarator: 1,
+            outerIIFEBody: 1,
+            MemberExpression: 1,
+            FunctionDeclaration: { parameters: 1, body: 1 },
+            FunctionExpression: { parameters: 1, body: 1 },
+            CallExpression: { arguments: 1 },
+            ArrayExpression: 1,
+            ObjectExpression: 1,
+            ImportDeclaration: 1,
+            flatTernaryExpressions: false,
+            ignoreComments: false,
+            ignoredNodes: [
                 'TemplateLiteral *',
                 'JSXElement',
                 'JSXElement > *',
@@ -60,17 +83,11 @@ export default {
                 'FunctionExpression > .params[decorators.length > 0]',
                 'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
                 'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key',
-                ],
-                offsetTernaryExpressions: true,
+            ],
+            offsetTernaryExpressions: true,
             },
         ],
-        '@typescript-eslint/no-floating-promises': 'error',
-        'no-async-promise-executor': 'error',
-        'no-await-in-loop': 'error',
-        'no-promise-executor-return': 'error',
-        'no-return-await': 'error',
-        '@typescript-eslint/await-thenable': 'error',
-        '@typescript-eslint/no-misused-promises': 'error',
-        '@typescript-eslint/promise-function-async': 'error',
-    }
-}
+        'object-shorthand': ['error', 'never'],
+        },
+    },
+);
